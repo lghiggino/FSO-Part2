f5f020f8d480cde038b10a62a79194af78854d38
lghiggino <lghiggino@gmail.com>
2022-10-20 08:14:04 -0300

    api baseUrl is set via process.env

--- Notes/src/services/notes.js
+++ Notes/src/services/notes.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-// const baseUrl = 'http://localhost:3001/api/notes'
-const baseUrl = "/api/notes";
+const baseUrl =
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
+    ? "http://localhost:3001/api/notes"
+    : "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
